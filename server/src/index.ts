0210b08b250058bbf8fbbf1a2c074bc0ed7a18df
Chloe <art.cchu@gmail.com>
2022-03-22 21:34:35 -0400

    login-and-validation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -15,11 +17,12 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
+    context: () => ({ em: orm.em }),
   });
-  await apolloServer.start()
+
   apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
